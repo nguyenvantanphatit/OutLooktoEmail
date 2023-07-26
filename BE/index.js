95063fcc0db95bab1632d5997307f3a6c26a4b38
Tan Phat <nguyenvantanphat.it@gmail.com>
2023-07-27 04:13:44 +0700

    Update

---
+++ BE/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import EmailSender from "./sendEmail.js";
+import serverless from "serverless-http";
+
+const router = express.Router();
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/send", async (req, res) => {
+  try {
+    const { email, subject, message } = req.body;
+    EmailSender({
+      email,
+      subject,
+      message,
+    });
+    res.json({ msg: "Your message sent successfully" });
+  } catch (error) {
+    res.status(404).json({ msg: "Error ❌" });
+  }
+});
+
+app.listen(8080, () => {
+  console.log("Connected to server!");
+});
+
+export default serverless(app);
